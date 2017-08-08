f4bdcbfaa111b26a6cd12c150df9c456bf7d5bdf
Seifon <Seifon@163.com>
2017-08-08 22:04:29 +0800

    项目POJO

---
+++ src/main/java/com/youu/youu/manager/constant/Constants.java
@@ -0,0 +1,12 @@
+package com.youu.youu.manager.constant;
+
+public class Constants {
+
+    // 登录后的用户
+    public static final String LOGIN_USER = "loginUser";
+    // 用户分页-每页大小
+    public static final int PAGE_SIZE = 6;
+    // 设置重置密码链接有效期为30分钟;
+    public static final long RESET_PASSWORD_VALID_TIME = 30 * 60 * 1000;
+
+}
